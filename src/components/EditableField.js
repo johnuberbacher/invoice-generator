540c0e84d7e5da71765b287aea9e4e9b4fc666b0
John Uberbacher <johnuberbacher@gmail.com>
2022-10-12 11:13:47 -0600

    Bug fixes

--- src/components/EditableField.js
+++ src/components/EditableField.js
@@ -8,7 +8,7 @@ class EditableField extends React.Component {
     return (
       <InputGroup className="my-1 flex-nowrap">
         {
-          this.props.cellData.leading != null &&
+          this.props.cellData.leading !== null &&
           <InputGroup.Text
             className="bg-light fw-bold border-0 text-secondary px-2">
             <span className="border border-2 border-secondary rounded-circle d-flex align-items-center justify-content-center small" style={{width: '20px', height: '20px'}}>
