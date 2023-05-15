e1049d023850dc80795ea9961c54f25b6a6b1e6c
Indu0903 <103114245+Indu0903@users.noreply.github.com>
2023-05-15 23:18:58 +0530

    Create Nodes.js

---
+++ src/Nodes.js
@@ -0,0 +1,9 @@
+export default function Notes() {
+    return (
+        <>
+            <section className="mb-5">
+                <p>Notes to the client...</p>
+            </section>
+        </>
+    )
+}
