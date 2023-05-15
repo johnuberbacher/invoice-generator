2660b41e88db8045ae04b322fcf538efe6c9fe95
Indu0903 <103114245+Indu0903@users.noreply.github.com>
2023-05-15 23:17:24 +0530

    Create Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <ul className="flex flex-wrap items-center justify-center">
+                    <li><span className="font-blod">Your Name:</span> Indu Sree</li>
+                    <li><span className="font-blod">Your Email:</span> info@gmail.com</li>
+                    <li><span className="font-blod">Phone number:</span> 9154867699</li>
+                    <li><span className="font-blod">Bank:</span> Bank Account</li>
+                    <li><span className="font-blod">Account holder:</span> Indu Sree</li>
+                    <li><span className="font-blod">Account number:</span> 123 456 789</li>
+                    <li><span className="font-blod">Website:</span> https://info.com</li>
+                </ul>
+            </footer>
+        </>
+    )
+}
