e222df14961a69c5004a45479b47370d54829844
Indu0903 <103114245+Indu0903@users.noreply.github.com>
2023-05-15 23:17:43 +0530

    Create Dates.js

---
+++ src/Dates.js
@@ -0,0 +1,13 @@
+export default function Dates() {
+    return (
+        <>
+            <article className="my-5 flex items-end justify-end">
+                <ul>
+                    <li><span className="font-bold">Incoicer number: </span></li>
+                    <li><span className="font-bold">Invoice Date: </span></li>
+                    <li><span className="font-bold">Due Date: </span></li>
+                </ul>
+            </article>
+        </>
+    )
+}
