b3627f00821ecb4b057f53acc663cc990bdc3509
Indu0903 <103114245+Indu0903@users.noreply.github.com>
2023-05-15 23:40:29 +0530

    Create Details.js

---
+++ src/Details.js
@@ -0,0 +1,10 @@
+export default function MainDetails() {
+    return (
+        <>
+            <section className="flex flex-col items-center justify-end">
+                <h2 className="text-xl uppercase">Your Name</h2>
+                <p>Your Address</p>
+            </section>
+        </>
+    )
+}
