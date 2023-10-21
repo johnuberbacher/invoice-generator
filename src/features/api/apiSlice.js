3b94ab95ff814ef478ac74e9d18c0b557c7da170
itsjustkarthik <itsjustkarthik@gmail.com>
2023-10-21 11:35:57 +0530

    process env

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const pokemonApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5555/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_BASE_URL }),
   endpoints: (builder) => ({
     getPokemonByName: builder.query({
       query: (name) => `invoices`,
